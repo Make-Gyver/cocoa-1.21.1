819cb2127f1c4524dede812ee39cecbecea6a54a
Ben <9918452+Make-Gyver@users.noreply.github.com>
2024-10-06 18:48:15 +0200

    Initial commit

---
+++ src/main/java/com/maxim/CocoaClient.java
@@ -0,0 +1,10 @@
+package com.maxim;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CocoaClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/maxim/CocoaDataGenerator.java
@@ -0,0 +1,11 @@
+package com.maxim;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CocoaDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
